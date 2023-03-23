f0601283c3d31f62e49b799468cae9fd68d8317c
Chieh Kung <35398659+g0ngjie@users.noreply.github.com>
2023-03-23 13:55:30 +0800

    init: demo

---
+++ Dockerfile
@@ -0,0 +1,1 @@
+FROM alrale/oh-my-docker:latest
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	. "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+	bot, err := NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
+	if err != nil {
+		panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	// Optional: wait for updates and clear them if you don't want to handle
+	// a large backlog of old messages
+	time.Sleep(time.Millisecond * 500)
+	updates.Clear()
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
+
+}
